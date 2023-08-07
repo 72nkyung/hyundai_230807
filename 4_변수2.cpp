b7f404787937e5efff2549ce7ee4fa1041845ff1
Chansik Yun <chansigi@gmail.com>
2023-08-07 11:34:05 +0900

    .

---
+++ 4_변수2.cpp
@@ -0,0 +1,21 @@
+// 4_변수2.cpp
+#include <iostream>
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
+
+int main()
+{
+    // Copy Initialization(복사 초기화)
+    int n1 = 42;
+    int x1[3] = { 10, 20, 30 };
+    Point pt1 = { 10, 20 };
+
+    // Direct Initialization(직접 초기화)
+    int n2(42);
+    int x2[3] { 10, 20, 30 };
+    Point pt2 { 10, 20 };
+}
