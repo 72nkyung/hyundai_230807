58a02646b6eab3e3404ab418495b90af23f11094
Chansik Yun <chansigi@gmail.com>
2023-08-07 10:06:16 +0900

    .

--- 1_시작2.cpp
+++ 1_시작2.cpp
@@ -1,5 +1,22 @@
 // 1_시작2.cpp
 
+// C++ 표준 입출력 라이브러리가 있습니다.
+#include <iostream>
+// std::cout -> stdout
+// std::cin  -> stdin
+
+#include <cstdio>
+// printf -> stdout
+// scanf  -> scanf
+
 int main()
 {
+    int n = 42;
+    char c = 'A';
+    double d = 3.14;
+
+    std::printf("%d, %c, %lf\n", n, c, d);
+
+    std::cout << n << ", " << c << ", "
+              << d << std::endl;
 }
