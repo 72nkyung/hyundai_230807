21184871c536efab4399f32e49d909e3be4de2be
Chansik Yun <chansigi@gmail.com>
2023-08-07 13:11:42 +0900

    .

--- 4_변수3.cpp
+++ 4_변수3.cpp
@@ -19,7 +19,7 @@ int main()
 // 2. 의존성 있는 타입에 대해서 사용을 할 때,
 //    타입이 변경되더라도, 코드를 수정할 필요가 없습니다.
 // 3. C의 auto의 키워드를 다른 의미로 사용합니다.
-
+#if 0
 int main()
 {
     int x[3] = { 10, 20, 30 };
@@ -29,4 +29,17 @@ int main()
 
     // auto n;
     // - auto를 사용하기 위해서는 반드시 초기화가 필요합니다.
+}
+#endif
+
+// auto는 우항과 타입과 동일한 타입으로 추론되는 것이 아닙니다.
+int main()
+{
+    const int n = 42; // 상수 - const int
+
+    auto x = n; // auto -> int
+    x = 10;
+
+    const auto x2 = n; // const int
+    // x2 = 10; // 컴파일 오류!
 }
