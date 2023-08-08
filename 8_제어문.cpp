5daacfb279e217e28cee85fcb11752aa1a9e87f1
Chansik Yun <chansigi@gmail.com>
2023-08-08 10:33:45 +0900

    .

---
+++ 8_제어문.cpp
@@ -0,0 +1,18 @@
+// 8_제어문.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x[5] = { 1, 2, 3, 4, 5 };
+
+    for (int i = 0; i < sizeof(x) / sizeof(x[0]); ++i) {
+        cout << x[i] << endl;
+    }
+
+    // C++11, Ranged-for
+    //  : index가 필요하지 않을 때
+    for (auto e : x) {
+        cout << e << endl;
+    }
+}
