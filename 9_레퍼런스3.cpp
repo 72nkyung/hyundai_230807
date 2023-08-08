b8217fadd2f49a50ddca8e142491e2aa456e062e
Chansik Yun <chansigi@gmail.com>
2023-08-08 11:36:36 +0900

    .

--- 9_레퍼런스3.cpp
+++ 9_레퍼런스3.cpp
@@ -10,6 +10,7 @@ void foo(int* x)
 }
 #endif
 
+#if 0
 void foo(int (&r)[5])
 {
     cout << sizeof(r) << endl;
@@ -30,3 +31,29 @@ int main()
     // foo(x); // &x[0]
     foo(x);
 }
+#endif
+
+#if 0
+// Decay: 배열의 이름은 배열의 첫번째 원소의 시작 주소로 해석됩니다.
+// 예외
+// 1) sizeof
+// 2) &(주소)
+// 3) &(레퍼런스)
+int main()
+{
+    int x[3] = { 10, 20, 30 };
+    // x: int[3]
+    cout << sizeof(int[3]) << endl; // 12
+    cout << sizeof(x) << endl; // 12
+
+    cout << sizeof(&x[0]) << endl; // 8
+
+    int* p = x; // &x[0]
+
+    int(*p2)[3];
+    p2 = &x; // x: int[3], &x: int(*)[3]
+
+    int(&r)[3] = x;
+    cout << sizeof(r) << endl;
+}
+#endif
