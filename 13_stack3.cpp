718ee4446540248cb73f0aa92ccd77b3fac48418
Chansik Yun <chansigi@gmail.com>
2023-08-08 14:27:54 +0900

    .

--- 13_stack3.cpp
+++ 13_stack3.cpp
@@ -7,12 +7,19 @@ using namespace std;
 //   => 캡슐화: 상태 + 행위
 
 // "상태와 행위를 가지는 변수" => "객체(object)"
+//  - 객체의 상태는 멤버 함수를 통해서만 읽거나 변경될 수 있어야 합니다.
+//    "정보 은닉(Information Hiding)"
+//    : 접근 지정자
+//     1) public: 외부에서 접근이 가능합니다.
+//     2) private: 외부에서 접근이 불가능하고, 오직 멤버 함수를 통해서만 접근이 가능합니다.
 
 struct Stack {
+private:
     // 멤버 데이터(상태)
     int buff[10];
     int top;
 
+public:
     // 멤버 함수(상태를 조작하는 행위)
     void init()
     {
@@ -39,6 +46,8 @@ int main()
     s1.push(10);
     s1.push(20);
     s1.push(30);
+
+    // s1.top = 0;
 
     cout << s1.pop() << endl;
     cout << s1.pop() << endl;
