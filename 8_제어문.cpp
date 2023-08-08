1a5fede82c930a2c356e11eb12967a106eb13e32
Chansik Yun <chansigi@gmail.com>
2023-08-08 10:40:54 +0900

    .

--- 8_제어문.cpp
+++ 8_제어문.cpp
@@ -3,6 +3,7 @@
 #include <string>
 using namespace std;
 
+#if 0
 int main()
 {
     int x[5] = { 1, 2, 3, 4, 5 };
@@ -20,5 +21,49 @@ int main()
     std::string s = "hello";
     for (auto e : s) {
         cout << e << endl;
+    }
+}
+#endif
+
+struct File { };
+
+File* OpenFile(std::string filename)
+{
+    // 실패할 경우, NULL을 반환합니다.
+    return NULL;
+}
+
+int Process()
+{
+    // 실패할 경우, 0이 아닌값을 반환합니다.
+    return -1;
+}
+
+// 실패 가능성이 있는 함수를 사용하면, 반드시 반환값을 체크해야 합니다.
+#if 0
+int main()
+{
+    File* ret = OpenFile("a.txt");
+    if (ret == NULL) {
+        cout << "File open error..." << endl;
+    }
+
+    int ret2 = Process();
+    if (ret2 != 0) {
+        cout << "Process failed..." << endl;
+    }
+}
+#endif
+
+// if statement with initializer, C++17
+// => 조건문에서만 유효한 변수를 정의할 수 있습니다.
+int main()
+{
+    if (File* ret = OpenFile("a.txt"); ret == NULL) {
+        cout << "File open error..." << endl;
+    }
+
+    if (int ret = Process(); ret != 0) {
+        cout << "Process failed..." << endl;
     }
 }
