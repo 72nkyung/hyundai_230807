e7e9833c20f3cd7839fdcd840a120c6f0469ae85
Chansik Yun <chansigi@gmail.com>
2023-08-08 14:08:45 +0900

    .

---
+++ 13_stack.cpp
@@ -0,0 +1,32 @@
+// 13_stack.cpp
+#include <iostream>
+using namespace std;
+
+// 스택을 만들어봅시다.
+// : 가장 마지막에 들어간 요소가 가장 먼저 나옵니다.
+//   LIFO(Last In First Out)
+
+// 1. 전역 변수 + 전역 함수
+int buff[10];
+int top = 0;
+
+void push(int n)
+{
+    buff[top++] = n;
+}
+
+int pop()
+{
+    return buff[--top];
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    cout << pop() << endl;
+    cout << pop() << endl;
+    cout << pop() << endl;
+}
