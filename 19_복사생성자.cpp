fb52bc4e0768af1bef76b3197696fba0553e28fd
Chansik Yun <chansigi@gmail.com>
2023-08-09 13:31:11 +0900

    .

--- 19_복사생성자.cpp
+++ 19_복사생성자.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 // 3. 사용자가 자신과 동일한 타입의 객체를 인자로 받는 생성자(복사 생성자)를 제공하지 않으면
 //    컴파일러는 자동으로 멤버 데이터를 복사하는 복사 생성자를 제공합니다.
-
+#if 0
 class Point {
     int x;
     int y;
@@ -45,3 +45,26 @@ int main()
 
     pt2.Print();
 }
+#endif
+
+#if 0
+class Sample1 { };
+// 1) 아무일도 수행하지 않는 기본 생성자
+// 2) 멤버 복사하는 복사 생성자
+
+class Sample2 {
+public:
+    Sample2(const Sample2& rhs) { }
+};
+// 컴파일러는 기본 생성자를 제공하지 않습니다.
+// 사용자가 복사 생성자를 제공하였기 때문입니다.
+// 복사 생성자도 생성자이기 때문입니다.
+
+int main()
+{
+    Sample1 s1;
+    Sample1 s2 = s1;
+
+    // Sample2 x; // 컴파일 오류!
+}
+#endif
