ba28272276d672e3d2ff93d76a23c43f0df665aa
Chansik Yun <chansigi@gmail.com>
2023-08-09 16:09:48 +0900

    .

--- 20_객체복사6.cpp
+++ 20_객체복사6.cpp
@@ -1,14 +1,16 @@
 // 20_객체복사6.cpp
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Sample {
 public:
     Sample() { }
     Sample(const Sample& rhs) { cout << "Sample(const Sample&)" << endl; }
-    // Sample(Sample&& rhs) { cout << "Sample(Sample&&)" << endl; }
+    Sample(Sample&& rhs) { cout << "Sample(Sample&&)" << endl; }
 };
 
+// std::move - lvalue를 rvalue로 변경할 수 있습니다.
 int main()
 {
     Sample s1;
