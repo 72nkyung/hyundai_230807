32d25efd06304ad23b1ea4255021993d37171100
Chansik Yun <chansigi@gmail.com>
2023-08-09 13:12:48 +0900

    .

--- 18_초기화리스트2.cpp
+++ 18_초기화리스트2.cpp
@@ -36,6 +36,12 @@ public:
         , rightBottom { 30, 40 }
     {
     }
+
+    Rect(int x1, int y1, int x2, int y2)
+        : leftTop { x1, y1 }
+        , rightBottom { x2, y2 }
+    {
+    }
 };
 
 int main()
